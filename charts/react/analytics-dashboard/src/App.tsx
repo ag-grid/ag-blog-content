6737862d59fcea82bb171f3c6a16fca9f0a66aa6
shadid12 <shadidhaque2012@gmail.com>
2025-03-17 00:41:50 -0400

    table added

--- charts/react/analytics-dashboard/src/App.tsx
+++ charts/react/analytics-dashboard/src/App.tsx
@@ -1,7 +1,9 @@
 import { useStockQuote } from './hooks/useStockQuote';
 import stocks from "../stocks.json";
+import StockTable from './components/StockTable';
 
 function App() {
+
   const tickers = stocks.map((stock) => stock.ticker).join(",");
   const { data, loading, error, refetch } = useStockQuote(tickers);
 
@@ -9,11 +11,20 @@ function App() {
 
   if (error) return <p>Error: {error.message}</p>;
 
-  console.log(data);
+  // This calculation can be done in the backend in a real-world application
+  const stocksWithBuyPrice = stocks.map((stock) => {
+    const currentStock = data?.find(
+      (item) => item.symbol === stock.ticker
+    );
+    return {
+      ...stock,
+      current_price: currentStock?.price || 0,
+    };
+  });
 
   return (
     <>
-      <div>Test</div>
+      <StockTable stocks={stocksWithBuyPrice} />
     </>
   )
 }
---
+++ charts/react/analytics-dashboard/src/components/StockTable.tsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import { AgGridReact } from "ag-grid-react";
+import { 
+  ModuleRegistry,
+  ClientSideRowModelModule, 
+  PaginationModule, 
+  NumberFilterModule, 
+  TextFilterModule, 
+  ColDef 
+} from "ag-grid-community";
+
+ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+  PaginationModule,
+  NumberFilterModule,
+  TextFilterModule
+]);
+
+type StockData = {
+  current_price: number;
+  company_name: string;
+  ticker: string;
+  amount: number;
+  entry_price: number;
+  total_price: number;
+}
+
+const StockTable = ({stocks} : {
+  stocks: StockData[]
+}) => {
+
+
+  const [columnDefs] = useState<ColDef<StockData>[]>([
+    {
+      headerName: "Company Name",
+      field: "company_name",
+    },
+    {
+      headerName: "Ticker",
+      field: "ticker",
+    },
+    {
+      headerName: "Shares",
+      field: "amount",
+      filter: "agNumberColumnFilter",
+    },
+    {
+      headerName: "Entry Price",
+      field: "entry_price",
+      filter: "agNumberColumnFilter",
+    },
+    {
+      headerName: "Current Price",
+      field: "current_price",
+      filter: "agNumberColumnFilter",
+    },
+    {
+      headerName: "Total Value",
+      valueGetter: (params) => params.data ? 
+          (params.data.amount * params.data.current_price).toFixed(2) : 0,
+      filter: "agNumberColumnFilter",
+    },
+	]);
+
+  const defaultColDef = {
+    flex: 1,
+    resizable: true,
+    floatingFilter: true,
+    filter: true,
+  };
+
+  const gridOptions = {
+    enableCharts: true,
+    cellSelection: true,
+  };
+
+  return (
+    <div>
+      <AgGridReact
+        rowData={stocks}
+        columnDefs={columnDefs}
+        defaultColDef={defaultColDef}
+        pagination={true}
+        paginationPageSize={10}
+        domLayout="autoHeight"
+        gridOptions={gridOptions}
+      />
+    </div>
+  );
+};
+
+export default StockTable;
